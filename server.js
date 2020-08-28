a083b67d1176bc499a6c7fa0cfcc74fd60f43994
Katie Lewis <33774231+kayjlou@users.noreply.github.com>
2020-08-27 19:32:05 -0700

    deleted node

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 3001;
 
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(require('./controllers/'));
 
 
 app.listen(PORT, () => {
